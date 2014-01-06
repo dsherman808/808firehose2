91541b98957250195d8595fe0ac42a7ab2ace1d6
d <d@dhcp-168-105-84-53.wireless.manoa.hawaii.edu>
2014-01-05 17:37:18 -1000

    second app firehose

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Embeddr::Application.config.secret_key_base = '7fa137c29195736b052939f82679ea7a95364c53d3666debbb1dca2dc38d89113d8dd3dbc9efd6cf87003ab64f1b8e5d9fee936e3859137c60681d4f2c998405'
